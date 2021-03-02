161a31abaaf28314ce27ef75523e34ed7396f757
aob3 <74620762+aob3@users.noreply.github.com>
2021-03-02 15:40:18 -0500

    update demo python env

--- demo_prep_devenv.sh
+++ demo_prep_devenv.sh
@@ -2,7 +2,7 @@
 
 echo
 echo
-echo "~~~~~~demo prep ec2-ami"
+echo "~~~~~~demo prep python en for ec2-ami"
 date
 echo
 echo "~~~~~~ as user: ec2-user"
@@ -21,15 +21,15 @@ cd ~
 virtualenv djangoenv2
 # # OR /usr/local/bin/virtualenv djangoenv
 
-# # creates env in: ~/djangoenv/ .
+# creates env in: ~/djangoenv/ .
 source ~/djangoenv/bin/activate
 pip3 install --upgrade Django==2.*
 python -m django --version
 
-# # to exit env
+# to exit env
 deactivate
 
-# # create second env - for dev work
+# create second env - for dev work
 virtualenv djangoenv2
 source ~/djangoenv2/bin/activate
 pip3 install --upgrade Django
@@ -45,10 +45,8 @@ wget https://kojipkgs.fedoraproject.org//packages/sqlite/3.8.11/1.fc21/x86_64/sq
 sudo yum install sqlite-3.8.11-1.fc21.x86_64.rpm -y
 sqlite3 --version
 
-# # to exit
+# to exit
 deactivate
 
 #### to get in dev env the run command:
 source ~/djangoenv2/bin/activate
-
-
